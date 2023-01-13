9f2009a969420d1f668647ec4d94f3e51d147420
Alexander Mc Alister <x19391041@student.ncirl.ie>
2023-01-13 15:51:22 +0000

    Devise implimented, Root URL setup - devise makes it so users must sign into the application before viewing anything as a layer of security

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :bookings
-  get 'page/home'
+  #get 'page/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+#	
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'page#home'
 end
